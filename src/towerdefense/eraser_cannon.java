fb029e97d670adb5ced206eaf5e5cc4e01372ab2
ebh5067 <ebh5067@206-L20.up.ist.local>
2015-09-16 20:25:40 -0400

    Helllloooooo

--- src/towerdefense/eraser_cannon.java
+++ src/towerdefense/eraser_cannon.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package towerdefense;
 
-/**
- *
- * @author mpw5216
- */
 public class eraser_cannon {
+    float monay;
     
+    private int setMoney(){
+        
+    }
+    
+    private int setDamge(){
+        
+    }
 }
