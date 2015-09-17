e0511909bcdac9f0f6b837bc5c0120bf8e1508f0
mpw5216 <mpw5216@206-L19.up.ist.local>
2015-09-16 20:07:44 -0400

    enemies,level,weapons #2

--- src/towerdefense/Enemies.java
+++ src/towerdefense/Enemies.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package towerdefense;
 
-/**
- *
- * @author mpw5216
- */
 public class Enemies {
     
     int enemy_number;
